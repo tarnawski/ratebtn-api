4d0f7b66549eca856d6207fc805635bd62cb328f
Tomasz Tarnawski <tarnawski27@gmail.com>
2019-09-20 14:53:03 +0200

    Basic implementation of date time

--- docker/mysql/init.sql
+++ docker/mysql/init.sql
@@ -3,11 +3,12 @@ DROP TABLE IF EXISTS `vote`;
 CREATE TABLE `vote` (
   `identity` VARCHAR(36) NOT NULL UNIQUE,
   `url` VARCHAR(255) NOT NULL,
-  `rate` INTEGER NOT NULL
+  `rate` INTEGER NOT NULL,
+  `created_at` datetime NOT NULL
 );
 
-INSERT INTO vote (identity, url, rate) VALUES ('cf3b433f-a395-465b-8705-abe0ba94f106', 'www.example.com', 2);
-INSERT INTO vote (identity, url, rate) VALUES ('2b7363a8-b3e4-421c-ad25-94e936c8562f', 'www.example.com', 3);
-INSERT INTO vote (identity, url, rate) VALUES ('b354dcd9-f7ae-415b-93fb-6084be35893d', 'www.example.com', 5);
-INSERT INTO vote (identity, url, rate) VALUES ('88193910-092a-43d4-83fc-196dc6340cc5', 'www.example.com', 2);
-INSERT INTO vote (identity, url, rate) VALUES ('1836a1da-9061-41bf-91af-967101fd2cda', 'www.example.com', 3);
+INSERT INTO vote (identity, url, rate, created_at) VALUES ('cf3b433f-a395-465b-8705-abe0ba94f106', 'www.example.com', 2, '2019-06-17 18:24:21');
+INSERT INTO vote (identity, url, rate, created_at) VALUES ('2b7363a8-b3e4-421c-ad25-94e936c8562f', 'www.example.com', 3, '2019-06-17 18:24:21');
+INSERT INTO vote (identity, url, rate, created_at) VALUES ('b354dcd9-f7ae-415b-93fb-6084be35893d', 'www.example.com', 5, '2019-06-17 18:24:21');
+INSERT INTO vote (identity, url, rate, created_at) VALUES ('88193910-092a-43d4-83fc-196dc6340cc5', 'www.example.com', 2, '2019-06-17 18:24:21');
+INSERT INTO vote (identity, url, rate, created_at) VALUES ('1836a1da-9061-41bf-91af-967101fd2cda', 'www.example.com', 3, '2019-06-17 18:24:21');
--- tests/Integration/VoteTest.php
+++ tests/Integration/VoteTest.php
@@ -2,18 +2,22 @@
 
 namespace App\Tests\Functional;
 
+use App\Domain\Vote\Identity;
 use App\Application\Command\VoteCommand;
 use App\Application\Command\VoteCommandHandler;
 use App\Application\ServiceBus\CommandBus;
+use App\Infrastructure\Calendar\StubCalendar;
 use App\Infrastructure\Presistance\InMemoryVoteRepository;
 use PHPUnit\Framework\TestCase;
+use DateTimeImmutable;
 
 class VoteTest extends TestCase
 {
     public function testAddVote(): void
     {
         $voteRepository = new InMemoryVoteRepository();
-        $voteCommandHandler = new VoteCommandHandler($voteRepository);
+        $calendar = new StubCalendar(new DateTimeImmutable('2019-06-17 18:24:21'));
+        $voteCommandHandler = new VoteCommandHandler($voteRepository, $calendar);
 
         $commandBus = new CommandBus();
         $commandBus->register($voteCommandHandler);
@@ -21,6 +25,11 @@ class VoteTest extends TestCase
         $voteCommand = new VoteCommand('1c46e9ed-d03a-4103-a3f2-2504c1f0052c', 'http://www.example.com', 3);
         $commandBus->handle($voteCommand);
 
-        $this->assertCount(1, $voteRepository->votes);
+        $result = $voteRepository->getByIdentity(Identity::fromString('1c46e9ed-d03a-4103-a3f2-2504c1f0052c'));
+
+        $this->assertEquals('1c46e9ed-d03a-4103-a3f2-2504c1f0052c', $result->getIdentity()->asString());
+        $this->assertEquals('http://www.example.com', $result->getUrl()->asString());
+        $this->assertEquals('2019-06-17 18:24:21', $result->getCreateAt()->format('Y-m-d H:i:s'));
+        $this->assertEquals(3, $result->getRate()->asInteger());
     }
 }
