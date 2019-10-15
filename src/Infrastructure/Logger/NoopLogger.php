d9cb75b67fe02578acc883d2b3e1a42ae4e81768
Tomasz Tarnawski <tarnawski27@gmail.com>
2019-10-15 20:27:06 +0200

    Add NullLogger to use in tests

---
+++ src/Infrastructure/Logger/NoopLogger.php
@@ -0,0 +1,16 @@
+<?php declare(strict_types=1);
+
+namespace App\Infrastructure\Logger;
+
+use App\Application\LoggerInterface;
+
+class NoopLogger implements LoggerInterface
+{
+    /**
+     * @inheritDoc
+     */
+    public function log(int $level, string $message, array $context = []): void
+    {
+        // This logger do nothing.
+    }
+}
